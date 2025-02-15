3bc446f04b97804af34542d6d7f7ac33ae3ed11b
PUJARI Akash <akashpuj@corp.idemia.com>
2025-02-15 18:29:44 +0530

    added JwtUtil,SecurityConfig,JwtAuthenticationFilter,CustomerUserDetailService and success fully bypassed jwt authentication for requests like user/signup,user/forgeotpassword,user/login path

---
+++ com.inn.cafe/src/main/java/com/inn/cafe/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.inn.cafe.jwt;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+    Claims claims = null;
+    private String userName = null;
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getServletPath().matches("/user/Login|/user/signup|/user/forgotpassword")) {
+            filterChain.doFilter(request, response);
+        } else {
+            String authorizationHeader = request.getHeader("Authorization");
+            String token = null;
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                token = authorizationHeader.substring(7);
+                userName = jwtUtil.extractUserName(token);
+                claims = jwtUtil.extrctAllClaims(token);
+
+            }
+
+            if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = customerUserDetailsService.loadUserByUsername(userName);
+                if (jwtUtil.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    public boolean isAdmin() {
+        return "admin".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public boolean isUser() {
+        return "user".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+}
